e3a74cf9473ccd5b15170a630673587f095832d8
Lorenzo Rapetti <lorenzo.rapetti.94@gmail.com>
2018-11-16 10:18:24 +0100

    Add unused variable checks

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,4 @@
-import {
-  createStyles,
-  Theme,
-  withStyles,
-  WithStyles,
-  MuiThemeProvider,
-} from '@material-ui/core/styles';
+import { createStyles, withStyles, WithStyles, MuiThemeProvider } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import React, { useState, useEffect, FunctionComponent } from 'react';
 import Header from './layout/Header';
@@ -13,7 +7,7 @@ import Content from './layout/Content';
 import { TitleContext } from './context';
 import { themeDark, themeLight } from './themes';
 
-const styles = (theme: Theme) =>
+const styles = () =>
   createStyles({
     root: {
       display: 'flex',
--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,3 @@
-import CssBaseline from '@material-ui/core/CssBaseline';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
